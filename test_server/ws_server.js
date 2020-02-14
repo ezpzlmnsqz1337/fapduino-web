5852fee71ed6259f40265e14cfcfa6068abb3567
Zdeněk Braun <zdenek.braun@siemens.com>
2020-02-14 10:46:25 +0100

    Communication should be working

--- test_server/ws_server.js
+++ test_server/ws_server.js
@@ -8,5 +8,15 @@ wss.on('connection', ws => {
 
     ws.on('message', message => {
         console.log('message: ', message)
+        if(message.includes('arm:leftRight')) {
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    console.log('armData:' + message.substring(message.lastIndexOf(':')+1).split('|')[0] + '|150|160|30|closed')
+                    client.send('armData:' + message.substring(message.lastIndexOf(':')+1).split('|')[0] + '|150|160|30|closed')
+                }
+            })
+        }
     })
+
+    // setInterval(() => ws.send('armData:90|150|160|30|closed'), 2000)
 })
